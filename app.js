5d6dd6221c6f9a8526ba08688a1b26b9db05f62a
owais888 <owaisashrafi888@gmail.com>
2020-10-22 01:21:19 +0500

    My portfolio

---
+++ app.js
@@ -0,0 +1,7 @@
+function gym(){
+    //    gym=document.getElementById("abc")
+       document.getElementById("abc").innerHTML = result;
+
+       var result = str.link("file:///C:/Users/hp/Desktop/gym-form/index.html");
+
+}
